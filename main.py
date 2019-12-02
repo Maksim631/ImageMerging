1fc3b73245661c1e5a0b7b447e9215f3f2f3bf41
Maksim631 <mar.maksim63@gmail.com>
2019-12-03 00:28:28 +0400

    update lab_array result

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from fourier import get_merge_parameters, merge_with_parameters
 
 def get_str_index(i):
     strI = str(i)
-    if i < 10:
-        strI = "0" + strI
-    if i < 100:
-        strI = "0" + strI
+    # if i < 10:
+    #     strI = "0" + strI
+    # if i < 100:
+    #     strI = "0" + strI
     return strI
 
 
@@ -16,25 +16,25 @@ translations = []
 current_translation_x = 0
 current_translation_y = 0
 
-i = 10
+i = 0
 
-result = Image.open('img/IX-01-61737_0029_0' + get_str_index(i) + '.JPG')
+result = Image.open('lab_images/cyl_image' + get_str_index(i) + '.png')
 
-while i > 4:
-    img1 = Image.open('img/IX-01-61737_0029_0' + get_str_index(i) + '.JPG')
-    img2 = Image.open('img/IX-01-61737_0029_0' + get_str_index(i + 1) + '.JPG')
-    translations.append(get_merge_parameters(img1, img2))
+while i < 24:
+    lab_images1 = Image.open('lab_images/cyl_image' + get_str_index(i) + '.png')
+    lab_images2 = Image.open('lab_images/cyl_image' + get_str_index(i + 1) + '.png')
+    translations.append(get_merge_parameters(lab_images1, lab_images2))
     print(i)
-    i -= 1
+    i += 1
 
-i = 9
+i = 1
 index = 0
-while i > 3:
+while i < 25:
     current_translation_x += translations[index][0]
     current_translation_y += translations[index][1]
-    result = merge_with_parameters(result, Image.open('img/IX-01-61737_0029_0' + get_str_index(i) + '.JPG'),
+    result = merge_with_parameters(result, Image.open('lab_images/cyl_image' + get_str_index(i) + '.png'),
                                    (current_translation_x, current_translation_y))
     index += 1
-    i -= 1
+    i += 1
 pyplot.imshow(result)
 pyplot.show()
