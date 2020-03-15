9306937a5dae38d70c6a2ded36d50d4aff9d3842
Maksim631 <mar.maksim63@gmail.com>
2020-03-15 21:14:27 +0400

    Another sun images

--- image_merging_bot/handler.py
+++ image_merging_bot/handler.py
@@ -1,2 +1,7 @@
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
+
+
+@permission_classes((AllowAny,))
 def handle(request):
     print("132313")
