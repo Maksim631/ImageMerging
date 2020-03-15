25942af82853976014f53fce34cb89ea1d759d17
Maksim631 <mar.maksim63@gmail.com>
2020-03-15 21:33:51 +0400

    Request simple handler

--- image_merging_bot/handler.py
+++ image_merging_bot/handler.py
@@ -1,7 +1,34 @@
+import json
 from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import AllowAny
+import os
+import requests
+
+TOKEN = os.environ['TELEGRAM_TOKEN']
+BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
+
 
 @api_view(['POST'])
 @permission_classes((AllowAny,))
 def handle(request):
-    print("132313")
+    print("start")
+    print(request)
+    try:
+        data = json.loads(request["body"])
+        message = str(data["message"]["text"])
+        chat_id = data["message"]["chat"]["id"]
+        first_name = data["message"]["chat"]["first_name"]
+
+        response = "Please /start, {}".format(first_name)
+
+        if "start" in message:
+            response = "Hello {}".format(first_name)
+
+        data = {"text": response.encode("utf8"), "chat_id": chat_id}
+        url = BASE_URL + "/sendMessage"
+        requests.post(url, data)
+        print("SUCCESS")
+    except Exception as e:
+        print(e)
+
+    return {"statusCode": 200}
